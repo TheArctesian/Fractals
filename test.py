c0503849066ad82a0bb5ce55e02fe61d76b85c68
TheArctesian <stephen.d.okita@gmail.com>
2022-01-20 14:00:18 +0800

    some extra files

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ turtle.bgcolor('black')
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x//100 + 1)
-    t.forward(x)
-    t.left(30)
+    t.speed(100);
+    t.forward(x/10 +1)
+    t.left(6)
+    t.dot(5);
